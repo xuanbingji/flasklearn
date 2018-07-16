77fdb62fb4e518d51317b81e89a722fce0966c29
xuanbingji <hjunzhen@163.com>
2018-07-16 19:39:45 +0800

    requirement

--- hello.py
+++ hello.py
@@ -5,14 +5,44 @@ from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'sqlite:///' + os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+db = SQLAlchemy(app)
+
 moment=Moment(app)
+
 bootstrap = Bootstrap(app)
 app.config['SECRET_KEY'] = 'hard to guess string'
 
 class NameForm(FlaskForm):
     name = StringField('what is your name?', validators= [DataRequired()])
     submit = SubmitField('Submit')
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(64), unique = True)
+    users = db.relationship('User',backref ='role')
+
+    def __repr__(self):
+        return '<Role {}>'.format(self.name)
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(64),unique = True , index = True)
+    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    
+    def __repr__(self):
+        return '<User {}>'.format(slef.username)
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -33,6 +63,7 @@ def index():
         session['name']= form.name.data
         return redirect(url_for('index'))
     return render_template('index.html', form=form, name = session.get('name'))
+
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
