4102e39e509897b4629523e6fb353740605723f5
xuanbingji <hjunzhen@163.com>
2018-07-16 23:30:15 +0800

    SQLAlchemy use

--- hello.py
+++ hello.py
@@ -5,16 +5,17 @@ from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_script import Manager
 from flask_sqlalchemy import SQLAlchemy
 import os
-
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
+manager = Manager(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = \
     'sqlite:///' + os.path.join(basedir,'data.sqlite')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
@@ -26,11 +27,12 @@ app.config['SECRET_KEY'] = 'hard to guess string'
 class NameForm(FlaskForm):
     name = StringField('what is your name?', validators= [DataRequired()])
     submit = SubmitField('Submit')
+
 class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(64), unique = True)
-    users = db.relationship('User',backref ='role')
+    users = db.relationship('User', backref ='role', lazy ='dynamic')
 
     def __repr__(self):
         return '<Role {}>'.format(self.name)
@@ -39,10 +41,10 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(64),unique = True , index = True)
-    role_id = db.Column(db.Integer,db.ForeignKey('roles.id'))
+    role_id = db.Column(db.Integer ,db.ForeignKey('roles.id'))
     
     def __repr__(self):
-        return '<User {}>'.format(slef.username)
+        return '<User {}>'.format(self.username)    
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -63,7 +65,10 @@ def index():
         session['name']= form.name.data
         return redirect(url_for('index'))
     return render_template('index.html', form=form, name = session.get('name'))
-
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
+
+
+if __name__ == '__main__':
+    manager.run()
