b0f394696234a7559a7614d38b4cf600fd24b9e4
xuanbingji <hjunzhen@163.com>
2018-07-17 14:43:34 +0800

    user_role is not null

--- hello.py
+++ hello.py
@@ -42,9 +42,9 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(64),unique = True , index = True)
     role_id = db.Column(db.Integer ,db.ForeignKey('roles.id'))
-    
+
     def __repr__(self):
-        return '<User {}>'.format(self.username)    
+        return '<User {}>'.format(self.username)
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -59,12 +59,20 @@ def internal_server_error(e):
 def index():
     form = NameForm()
     if form.validate_on_submit():
-        old_name = session.get('name')
-        if old_name is not None and old_name != form.name.data:
-           flash('Looks like you have changed your name!')
-        session['name']= form.name.data
+        user = User.query.filter_by(username =form.name.data).first()
+        user_role = Role.query.filter_by(name='User').first()
+        if user is None:
+            user = User(username =form.name.data, role=user_role)
+            db.session.add(user)
+            db.session.commit()
+            session['known'] = False
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
+        form.name.data = ''
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name = session.get('name'))
+    return render_template('index.html', form=form, name =session.get('name'), known=session.get('known', False))
+
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
