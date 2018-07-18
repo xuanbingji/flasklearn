163a8997cdd6e855b330cfc6d4ba559626b89220
xuanbingji <hjunzhen@163.com>
2018-07-18 16:51:01 +0800

    large software

---
+++ app/main/views.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+from flask import render_template, session, redirect, url_for
+
+from . import main
+from .forms import NameForm
+from .. import db
+from ..models import User, Role
+
+@main.route('/', methods=['GET','POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()
+        user_role = Role.query.filter_by(name='User').first()
+        if user is None:
+            user = User(username=form.name.data, role=user_role)
+            db.session.add(user)
+            db.session.commit()
+            session['known'] = False
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
+        form.name.data = ''
+        return redirect(url_for('.index'))
+    return render_template('index.html',
+                            form =form,name=session.get('name'),
+                            known=session.get('known', False))
